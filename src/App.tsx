67ed67153b456b36b62616c1ae3369b71fb3c987
Chris Moss <hcmoss70@gmail.com>
2023-11-22 21:10:57 -0500

    got colors right and set up pong screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 function App() {
   return (
-    <div className="w-full">
-      <div className="w-full flex justify-center">Pong it up</div>
+    <div className="h-screen flex items-center justify-center bg-color-1">
+      <div className="relative w-96 h-48 bg-color-5 border-4 border-color-2">
+        {/* Paddle 1 */}
+        <div
+          className="absolute left-2/4 transform -translate-x-2/4 w-4 h-16 bg-color-2"
+          style={{ top: "50%" }}
+        ></div>
+
+        {/* Paddle 2 */}
+        <div
+          className="absolute left-full transform translate-x-2/4 w-4 h-16 bg-color-3"
+          style={{ top: "50%" }}
+        ></div>
+
+        {/* Ball */}
+        <div className="absolute left-2/4 transform -translate-x-2/4 -translate-y-2/4 w-4 h-4 bg-color-4 rounded-full"></div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{tsx,ts}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-1": "#7e4b27", // Brown
+        "color-2": "#98cba6", // Mint
+        "color-3": "#f1ab23", // Yellow
+        "color-4": "#e9861a", // Orange
+        "color-5": "#f6e9ac", // Beige
+      },
+    },
   },
   plugins: [],
 };
